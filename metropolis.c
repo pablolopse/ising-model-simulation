935762a79b0a5b029218d5b4752ef586ceab59ea
Pablo López <pablolopse@usal.es>
2026-02-12 19:17:15 +0100

    Initial Commit

---
+++ metropolis.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define DIMENSION 2
+#define SIZE 10
+#define SQUARES 1
+
+void main() {
+    printf("Simulacion de Metropolis\n");
+    printf("Dimension: %d\n", DIMENSION);
+
+    
+}
